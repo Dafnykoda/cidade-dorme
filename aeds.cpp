fd60ce37709fb1b88b8b7dfd4b74e12c9c0b713e
Victoribeiros <vh500499@gmail.com>
2026-06-03 14:46:16 -0300

    Add greeting message from Hugo

--- aeds.cpp
+++ aeds.cpp
@@ -5,6 +5,7 @@ int main() {
     cout << "=================================" << endl;
     cout << "   PROJETO DE EQUIPE INICIADO!   " << endl;
     cout << "=================================" << endl;
+    cout << "Hugo aqui"<<endl;
     
     return 0;
 }
